11fb7b0a311a4f2d1c2aae9434b35fa3e6a07194
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-10 13:26:03 +0100

    update index controller and cors policy

--- app/controllers/api/v1/dogs_controller.rb
+++ app/controllers/api/v1/dogs_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::DogsController < ApplicationController
+  # path http://localhost:3000/api/v1/dogs.json
   def index
+    @dogs = Dog.all
+    render json: @dogs
   end
 
   def show
   end
 
   def create
-    render json :@dog, status: :created, location: api_v1_dog_path(@dog)
   end
 end
