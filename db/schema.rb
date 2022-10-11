5391e8b857363d36b6057b11171394f486a79381
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-11 15:20:00 +0100

    update to postgresql db

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_10_102431) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "dogs", force: :cascade do |t|
     t.string "breed"
     t.string "weight_imperial"
