f3c8cf73698e8824d9b256300a9f9091e3cd04e9
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-11 14:35:13 +0100

    update controller to search if q param is present

--- app/controllers/api/v1/dogs_controller.rb
+++ app/controllers/api/v1/dogs_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::DogsController < ApplicationController
   # path http://localhost:3000/api/v1/dogs.json
   def index
-    @dogs = Dog.all
+    if params[:q].present?
+      @dogs = Dog.where('breed LIKE ?', "%#{params[:q]}%")
+    else
+      @dogs = Dog.all
+    end
     render json: @dogs, status: 200
   end
 
