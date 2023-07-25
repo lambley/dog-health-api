d9658542c6e81c4e7b73f40833075458f0241036
Aaron Lambley <aaron.lambley@gmail.com>
2023-07-25 14:56:53 +0100

    add pgsearch and setup model with pgsearch scope

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem "rails", "~> 7.0.4"
 
 # Use Postgres as the database for Active Record
 gem 'pg'
+gem 'pg_search'
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,8 @@
 class Dog < ApplicationRecord
+  include PgSearch::Model
+
+  pg_search_scope :search_by_breed, against: :breed, using: { tsearch: { prefix: true } }
+
+  pg_search_scope :search_by_characteristics, against: [:temperament, :breed_group], using: { tsearch: { prefix: true } }
+
 end
