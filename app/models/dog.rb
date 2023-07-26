82d4bc04b6894b1bace4c945419097501ea71851
Aaron Lambley <aaron.lambley@gmail.com>
2023-07-26 09:26:00 +0100

    add weighting to pg search scope

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,8 +1,19 @@
 class Dog < ApplicationRecord
   include PgSearch::Model
 
-  pg_search_scope :search_by_breed, against: :breed, using: { tsearch: { prefix: true } }
+  pg_search_scope :search_by_breed,
+                  against: :breed,
+                  using: {
+                     tsearch: { prefix: true }
+                  }
 
-  pg_search_scope :search_by_characteristics, against: [:temperament, :breed_group], using: { tsearch: { prefix: true } }
+  pg_search_scope :search_by_characteristics,
+                  against: {
+                    temperament: 'A',
+                    breed_group: 'B',
+                  },
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 
 end
